0380df5686adcd29926ad88b6d6a8cea804b5ef1
Muhammad Basil khan <142798048+MuhammadBasilKhan@users.noreply.github.com>
2023-10-25 07:06:37 -0700

    java stack DSA project

---
+++ Stack_project123.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.stack_project123;
+
+/**
+ *
+ * @author Basil Khan
+ */
+public class Stack_project123 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
